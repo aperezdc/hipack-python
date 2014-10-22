ea0ff9b573bded234cbd4a873df39ed12cf7bc87
Adrian Perez de Castro <aperez@igalia.com>
2014-10-22 08:23:26 +0300

    Make input in tests all be UTF-8
    wcfg always reads UTF-8 and writes back UTF-8, so the test suite has to take
    care of always feeding UTF-8 to the parser.

--- test/test_parser.py
+++ test/test_parser.py
@@ -16,7 +16,7 @@ class TestParser(unittest.TestCase):
 
     @staticmethod
     def parser(string):
-        return wcfg.Parser(six.BytesIO(six.b(string)))
+        return wcfg.Parser(six.BytesIO(string.encode("utf-8")))
 
     def check_numbers(self, numbers, type_):
         for item, result in numbers:
@@ -30,34 +30,34 @@ class TestParser(unittest.TestCase):
 
     def test_parse_valid_integer_numbers(self):
         numbers = (
-            ("1", 1),
-            ("+1", 1),
-            ("-2", -2),
-            ("123", 123),
-            ("+123", 123),
-            ("-123", -123),
+            (u"1", 1),
+            (u"+1", 1),
+            (u"-2", -2),
+            (u"123", 123),
+            (u"+123", 123),
+            (u"-123", -123),
         )
         self.check_numbers(numbers, six.integer_types)
 
     def test_parse_valid_hex_numbers(self):
         numbers = (
-            ("0x1", 1),
-            ("0X1", 1),
-            ("0xcafe", 0xCAFE),
-            ("0XCAFE", 0xCAFE),
-            ("0xCAFE", 0xCAFE),
-            ("0xCaFe", 0xCAFE),
-            ("0x1234", 0x1234),
-            ("0xC00FFEE", 0xC00FEE),
-            ("0xdeadbeef", 0xdeadbeef),
+            (u"0x1", 1),
+            (u"0X1", 1),
+            (u"0xcafe", 0xCAFE),
+            (u"0XCAFE", 0xCAFE),
+            (u"0xCAFE", 0xCAFE),
+            (u"0xCaFe", 0xCAFE),
+            (u"0x1234", 0x1234),
+            (u"0xC00FFEE", 0xC00FEE),
+            (u"0xdeadbeef", 0xdeadbeef),
         )
         self.check_numbers(numbers, six.integer_types)
 
     def test_parse_valid_octal_numbers(self):
         numbers = (
-            ("01", 01),
-            ("01234567", 01234567),
-            ("042", 042),
+            (u"01", 0o1),
+            (u"01234567", 0o1234567),
+            (u"042", 0o42),
         )
         self.check_numbers(numbers, six.integer_types)
 
@@ -83,7 +83,7 @@ class TestParser(unittest.TestCase):
             u"Güedángaños",
         )
         for item in identifiers:
-            ident = self.parser(item.encode("utf-8")).parse_identifier()
+            ident = self.parser(item).parse_identifier()
             self.assertEquals(item, ident)
             self.assertTrue(isinstance(ident, six.text_type))
 
